44188f1bd17f4ead3dddadc6e10968d663abe2eb
Pio Rasch-Halvorsen <pio@Pios-MBP.lan>
2023-03-04 16:06:28 +0100

    feat: add types for current weather response

--- src/api/types.ts
+++ src/api/types.ts
@@ -6,3 +6,37 @@ export type ReverseGeoResult = {
   country: string;
   state?: string;
 };
+
+export type CurrentWeatherResult = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  name: string;
+  main: {
+    feels_like: number;
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  visibility: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+};
