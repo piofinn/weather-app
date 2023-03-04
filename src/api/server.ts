8e4943627e74e4d4f0cbba382bb8ba40a3083ce0
Pio Rasch-Halvorsen <pio@Pios-MBP.lan>
2023-03-04 16:07:39 +0100

    fix: actually return API results, and type them correctly

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,9 +1,11 @@
+import { CurrentWeatherResult, ReverseGeoResult } from "./types";
+
 const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
-async function makeSimpleFetchRequest(
+async function makeSimpleFetchRequest<T>(
   input: RequestInfo | URL,
   init?: RequestInit
-) {
+): Promise<T> {
   const response = await fetch(input, init);
 
   if (!response.ok) {
@@ -13,14 +15,12 @@ async function makeSimpleFetchRequest(
   return response.json();
 }
 
-export const getLocationName = async (lat: string, lon: string, limit = 1) => {
-  makeSimpleFetchRequest(
+export const getLocationName = async (lat: string, lon: string, limit = 1) =>
+  makeSimpleFetchRequest<ReverseGeoResult>(
     `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid=${API_KEY}`
   );
-};
 
-export const getCurrentWeather = async (lat: string, lon: string) => {
-  makeSimpleFetchRequest(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+export const getCurrentWeather = async (lat: string, lon: string) =>
+  makeSimpleFetchRequest<CurrentWeatherResult>(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
   );
-};
