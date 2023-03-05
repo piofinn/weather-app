1f3db9dfa247c3f59f438fe4ac32bcba9b5b3d21
Pio Rasch-Halvorsen <pio@Pios-MBP.lan>
2023-03-05 16:06:09 +0100

    feat: add icons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,32 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export const Header: FC = () => (
-  <header className="flex flex-row bg-slate-800 py-8 px-4">
-    <Link className="" to="/">
-      ←
-    </Link>
-    <h1 className="flex-grow text-center text-xl text-slate-300 font-bold">
-      Dashboard
-    </h1>
-  </header>
-);
+export const Header: FC = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <header className="flex flex-row items-center bg-slate-800 py-8 px-4 text-slate-300">
+      {pathname !== "/" && (
+        <Link
+          title="Back to dashboard"
+          aria-label="Back to dashboard"
+          className="w-10 material-symbols-rounded text-[24px] hover:text-white"
+          to="/"
+        >
+          {"\ue5e0"}
+        </Link>
+      )}
+      <h1 className="flex-grow text-center text-xl text-slate-300 font-bold">
+        Dashboard
+      </h1>
+      <button
+        // TODO: Actually show settings
+        title="Show settings"
+        aria-label="Show settings"
+        className="w-10 h-10 material-symbols-rounded text-[24px] hover:text-white"
+      >
+        {"\ue8b8"}
+      </button>
+    </header>
+  );
+};
