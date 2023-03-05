db62a1d37d1cb2bde9a20e389c2cbf4cc1a58497
Pio Rasch-Halvorsen <pio@Pios-MBP.lan>
2023-03-05 11:45:22 +0100

    perf: throttle data fetching from API

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,11 +4,13 @@ import { getCurrentWeather, getLocationName } from "./server";
 export const useLocationName = (lat: string, lon: string, limit?: number) =>
   useQuery({
     queryKey: ["location", lat, lon, limit],
-    queryFn: async () => getLocationName(lat, lon, limit),
+    queryFn: () => getLocationName(lat, lon, limit),
+    staleTime: 60000, // Only fetch new data every minute
   });
 
 export const useCurrentWeather = (lat: string, lon: string) =>
   useQuery({
     queryKey: ["weather", lat, lon],
-    queryFn: async () => getCurrentWeather(lat, lon),
+    queryFn: () => getCurrentWeather(lat, lon),
+    staleTime: 60000, // Only fetch new data every minute
   });
